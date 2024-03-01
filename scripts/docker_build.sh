928cf82e999bc4ab6514798db4b2eccfee83a278
Kevin Barnard <kbarnard@mbari.org>
2024-03-01 09:53:27 -0800

    Improve docker build/push scripts to target different dockerfiles

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
-docker build -t mbari/lcm-websocket-server:latest .
+# Read the first argument from the command line. This will specify which image we are building:
+# - empty: docker/Dockerfile
+# - x: docker/Dockerfile.x
+
+# If no argument is provided, use the default Dockerfile
+
+DOCKEROPT=${1:-""}
+DOCKERFILE="Dockerfile"
+if [ -n "$DOCKEROPT" ]; then
+    DOCKERFILE="Dockerfile.$DOCKEROPT"
+fi
+# Check that the Dockerfile exists
+if [ ! -f "docker/$DOCKERFILE" ]; then
+    echo "Dockerfile $DOCKERFILE does not exist"
+    echo "Usage: $0 [DOCKERFILE EXTENSION]"
+    exit 1
+fi
+
+VERSION=$(grep '^version =' pyproject.toml | awk -F'"' '{print $2}')
+DOCKERTAG="$VERSION"
+if [ -n "$DOCKEROPT" ]; then
+    DOCKERTAG="$DOCKEROPT-$VERSION"
+fi
+
+DOCKERIMAGE="mbari/lcm-websocket-server:$DOCKERTAG"
+
+echo "Building docker image $DOCKERIMAGE"
+docker build -t $DOCKERIMAGE -f docker/$DOCKERFILE --build-arg VERSION=$VERSION .
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-docker push mbari/lcm-websocket-server:latest
+# Read the first argument from the command line. This will specify which image we are pushing.
+
+DOCKEROPT=${1:-""}
+VERSION=$(grep '^version =' pyproject.toml | awk -F'"' '{print $2}')
+DOCKERTAG="mbari/lcm-websocket-server:$VERSION"
+if [ -n "$DOCKEROPT" ]; then
+    DOCKERTAG="mbari/lcm-websocket-server:$DOCKEROPT-$VERSION"
+fi
+
+echo "Pushing docker image $DOCKERTAG"
+docker push $DOCKERTAG
