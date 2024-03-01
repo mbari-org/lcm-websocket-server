e9b3743d08b2ed584113b9857203272fb833e4da
Kevin Barnard <kbarnard@mbari.org>
2024-03-01 10:03:55 -0800

    Add script to update latest tags

---
+++ scripts/docker_update_latest_tags.sh
@@ -0,0 +1,11 @@
+VERSION=$(grep '^version =' pyproject.toml | awk -F'"' '{print $2}')
+
+EXTS=( "compas-lcmtypes" "dial" )
+
+docker tag mbari/lcm-websocket-server:$VERSION mbari/lcm-websocket-server:latest
+docker push mbari/lcm-websocket-server:latest
+
+for EXT in "${EXTS[@]}"; do
+    docker tag mbari/lcm-websocket-server:$VERSION-$EXT mbari/lcm-websocket-server:latest-$EXT
+    docker push mbari/lcm-websocket-server:latest-$EXT
+done
